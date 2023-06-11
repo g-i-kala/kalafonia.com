deea988028cc4e1def41e267a32b8888976acd99
g-i-kala <5star@wp.pl>
2023-06-11 10:59:51 +0200

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,136 @@
+//import assets
+
+//data from website export const xxx = [{object}]
+
+export const landingPage = [
+    {
+        id:'background',
+        img: background
+    },
+    {
+        id:'logo',
+        img: logo
+    },
+    {
+        id: 'title',
+        text: 'kalafonia'
+    },
+    {
+        id: 'subtitle',
+        text: 'electronic music production'
+    },
+    {
+        id: 'button',
+        text: 'explore',
+        link: '/bio'
+    },
+    {
+        id: 'button',
+        text: 'dance',
+        link: 'https://open.spotify.com/album/19K34vTAJFQLASLeYXiIdx?si=eF2HqKNTReaPIeO5EInsQA'
+
+    },
+];
+
+export const navLinks = [
+    {
+        id: 'projects',
+        title: 'projects',
+    },
+    {
+        id: 'bio',
+        title: 'bio',
+    },
+    {
+        id: 'offer',
+        title: 'offer',
+    },
+];
+
+export const bio = [
+    {
+        id: 'bio',
+        text: 'I\'m a freelance sound engineer and a music producer. Before "pandemic times" I cooperated with Stage Entertainment Touring Productions, Music Hall and Musical Theatre in Gdynia as a live sound mixer and sound designer. During the lockdowns I moved all my skills to the studio and started producing my own music. As a freelancer I concentrate on mixing and mastering. To stay fresh I\’ve started performing live. In my compositons I put big emphasis on arrangement minimalism, originality of sounds and the potential of translating my music into body motility. I\’m passionate about synthesisers and a geek of "sound toys" as I call all controllers and audio devices used in the studio and on stage.'
+    }
+
+];
+
+export const projects = [
+    {
+        id: 'kalafonia',
+        text: 'Electronic project, based on techno music. 3rd EP "Regen" will hit the streaming platforms in Autumn 2022.',
+        img: cover_regen
+    },
+    {
+        id: 'CrewCruLove',
+        text: 'Musical improvised duo with Joanna Duda. A mix of genres multiplies by emotions and inspiration. IDM.',
+        img: cover_ckl,
+        link: 'http://www.joannaduda.eu/'
+    },
+    {
+        id: 'hk+',
+        text: 'Musical duo with an electro direction. "Live Acts". From the sea and dreams.  ',
+        img: cover_hk
+    },
+    {
+        id: 'Eurozone Music - Kalafonia Deep House',
+        text: 'Music library is a truly independent Library Music and Publisher specializing in Eastern European music with a huge selection of extensive music genres for all types of high-quality productions at the highest technical and creative levels.',
+        img: cover_ezm209,
+        link: 'http://www.eurozonemusic.com/en/album/ez-209-kalafonia-deep-house/'
+    },
+    {
+        id: 'Eurozone Music - Chilled House Fever vol2',
+        text: 'Music library is a truly independent Library Music and Publisher specializing in Eastern European music with a huge selection of extensive music genres for all types of high-quality productions at the highest technical and creative levels.',
+        img: cover_ezm205,
+        link: 'http://www.eurozonemusic.com/en/album/ez-205-chilled-house-fever-vol-2/'
+    },
+    {
+        id: 'Eurozone Music - Chilled House Fever vol1',
+        text: 'Music library is a truly independent Library Music and Publisher specializing in Eastern European music with a huge selection of extensive music genres for all types of high-quality productions at the highest technical and creative levels.',
+        img: cover_ezm202,
+        link: 'http://www.eurozonemusic.com/en/album/ez-202-chilled-house-fever/'
+    },
+];
+
+export const socialMedia = [
+    {
+      id: "sm-1",
+      icon: facebook,
+      link: "http://www.facebook.com/kalafoniamusic",
+    },
+    {
+      id: "sm-2",
+      icon: instagram,
+      link: "http://www.instagram.com/_kalafonia_",
+    },
+    {
+      id: "sm-3",
+      icon: youtube,
+      link: "https://www.youtube.com/@kalafoniamusic",
+    },
+    {
+      id: "sm-4",
+      icon: spotify,
+      link: "https://open.spotify.com/artist/5CqjV77x6k2nNPJ5FVWuev?si=dBdqVPyURtayu2rV2m_tIA",
+    },
+    {
+      id: "sm-5",
+      icon: soundcloud,
+      link: "http://www.soundcloud.com/kalafonia",
+    },
+    {
+        id: "sm-6",
+        icon: bandcamp,
+        link: "http://kalafonia.bandcamp.com/",
+    },
+    {
+        id: "sm-7",
+        icon: message,
+        link: "mailto:kalafoniamusic@gmail.com",
+    },
+    
+  ];
+
+  export const footerText = [
+    "Copyright &copy; {new Date().getFullYear()} kalafonia. All rights reserved",
+]
